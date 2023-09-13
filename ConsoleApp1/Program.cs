3d4fa59d08cb133d470d4d576e7fdc96e347c325
saivignesh.p <saivignesh14012001@gmail.com>
2023-09-13 12:22:58 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Function1);
+            Thread t2 = new Thread(Function2);
+            //t1.IsBackground = true;
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine("I am exited");
+        }
+        
+        public static void Function1()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Execution from function 1 {i}");
+                Thread.Sleep(4000);
+            }
+
+
+        }
+        public static void Function2()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Execution from function 2 {i}");
+                Thread.Sleep(4000);
+            }
+            
+
+        }
+    }
+}
